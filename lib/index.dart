857730f1dfc4f83c953b3c2068b5891073c66773
Ozan Can Acar <ozancanacar1998@gmail.com>
2024-06-26 13:20:00 +0300

    first commit

---
+++ lib/index.dart
@@ -0,0 +1,17 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/simple_auth_flow/auth_3_create/auth3_create_widget.dart'
+    show Auth3CreateWidget;
+export '/simple_auth_flow/auth_3_login/auth3_login_widget.dart'
+    show Auth3LoginWidget;
+export '/simple_auth_flow/auth_3_phone/auth3_phone_widget.dart'
+    show Auth3PhoneWidget;
+export '/simple_auth_flow/auth_3_verify_phone/auth3_verify_phone_widget.dart'
+    show Auth3VerifyPhoneWidget;
+export '/simple_auth_flow/auth_3_forgot_password/auth3_forgot_password_widget.dart'
+    show Auth3ForgotPasswordWidget;
+export '/pages/profile/profile_widget.dart' show ProfileWidget;
+export '/pages/edit/edit_widget.dart' show EditWidget;
+export '/film_sorgu/film_sorgu_widget.dart' show FilmSorguWidget;
+export '/recomended_film/recomended_film_widget.dart' show RecomendedFilmWidget;
+export '/history2/history2_widget.dart' show History2Widget;
